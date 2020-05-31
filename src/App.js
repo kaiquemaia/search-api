2df8c5befe9d61dbbdcc6671ff166323debe2180
kaiquemaia <kaiquefeng1@gmail.com>
2020-05-30 22:09:35 -0300

    Criando o App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/global.css'
+
+export default function App() {
+    return (
+        <div className="container-search">
+            <div className="card-search">
+                <h1>Pesquise uma empresa</h1>
+                <form>
+                    <input type="text" placeholder="Pesquise uma empresa" />
+                    <button>Pesquisar</button>
+                </form>
+
+                <div className="card-sugestion">
+                    <h4>Nome da empresa</h4>
+                </div>
+            </div>
+
+            
+        </div>
+        
+    )
 }
-
-export default App;
